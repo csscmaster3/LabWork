c67a3635273efcfefe98a1d0acda83ea0f96ee54
unknown <kevin@varcasio.com>
2013-01-13 19:34:51 -0500

    Commit!

---
+++ Lab1Problem1.py
@@ -0,0 +1,17 @@
+# Lab1 Problem1
+# 13/01/2013
+# Kevin Varcasio
+# Get five number inputs and calculate the average
+
+#KV - Get five inputs from the user
+print("Please input 5 floats: ")
+a = float(input("Number 1: "))
+b = float(input("Number 2: "))
+c = float(input("Number 3: "))
+d = float(input("Number 4: "))
+e = float(input("Number 5: "))
+
+#KV - Calculate and output the average
+print("The average is:",(a+b+c+d+e)/5)
+
+ 
---
+++ Lab1Problem2.py
@@ -0,0 +1,15 @@
+# Lab1 Problem2
+# 13/01/2013
+# Kevin Varcasio
+# Get dimensions for a room and calculate the total area to be painted
+
+#KV - Get length, width, heigh
+print("Please input the room dimensions: ")
+l = float(input("Length: "))
+w = float(input("Width: "))
+h = float(input("Height: "))
+
+#KV - Calculate the total area to be painted
+print("The total area is:", (l*w*2)+(h*w*2)+(l*h*2))
+
+ 
